b00ebd930c65c13bb01c2b90a18e561ff0ea750e
mattiasoldani <mattiasoldani93@gmail.com>
2025-11-19 19:24:36 +0100

    polished DetectorConstruction methods

--- succosim/include/DetectorConstruction.hh
+++ succosim/include/DetectorConstruction.hh
@@ -32,8 +32,7 @@ private:
     // e.g. void ConstructCalo(G4LogicalVolume* worldLog);
 
     // all about isosceles trapezoids
-    class geomTrapezoid
-    {
+    class geomTrapezoid{
         public:
             geomTrapezoid(G4double r, G4double h, G4double theta) {
                 R = r; // R: central radial position, i.e. distance between the height mid-point and the point in which the prolongations of the two non-parallel sides meet
@@ -50,7 +49,7 @@ private:
                 // - L_mid: full width (i.e. horizontal length) at mid height
                 // - side: full length of one of the non-parallel sides
             }
-
+            
             void SetR(G4double r) {R = r; SetAllDerived();}
             void SetTheta(G4double theta) {Theta = theta; SetAllDerived();}
             void SetH(G4double h) {H = h; SetAllDerived();}
@@ -87,28 +86,8 @@ private:
             // calculate and get the (horizontal) distance between the full tile and the half tile
             G4double GetFullToHalfCentreOffset() {return L_mid/4;}
 
-            // reduce the horizontal dimensions to account for the fibre gaps
-            // only (some) horizontal quantities are affected
-            void AddHorGaps(G4double gapsize) {
-                GapHor = gapsize / cos(GetTheta() / 2);
-                isGaps = true;
-
-                L_b = GetDHor_b() - 2*GapHor;
-                L_t = GetDHor_t() - 2*GapHor;
-                SetDHor_mid(); // recalculate L_mid
-            }
-
-            //
-            void RmHorGaps() {
-                if (isGaps) {
-                    L_b = GetDHor_b() + 2*GapHor;
-                    L_t = GetDHor_t() + 2*GapHor;
-                    SetDHor_mid(); // recalculate L_mid
-
-                    GapHor = 0;
-                    isGaps = false;
-                } else {return;}
-            }
+            void AddHorGaps(G4double gapsize); // defined in DetectorConstruction.cc
+            void RmHorGaps(); // defined in DetectorConstruction.cc
 
         private:
             G4double pi = acos(-1);
@@ -142,87 +121,25 @@ private:
             G4double fside(G4double h, G4double theta) { 
                 return (h / sin(pi/2 - theta/2));
             }
-
     };
 
-    // create the full tile solid
-    G4VSolid* fShapeTileFull(
+    G4VSolid* fShapeTileFull( // defined in DetectorConstruction.cc
         geomTrapezoid* pGeom, 
         G4String name, 
         G4double dThk
-    ){
-        G4double dHor_b = pGeom->GetDHor_b();
-        G4double dHor_t = pGeom->GetDHor_t();
-        G4double dVer = pGeom->GetDVer();
+    );
 
-        G4String name_solid = name + "_Shape";
-        G4VSolid* shape = new G4Trd(name_solid, dHor_b/2, dHor_t/2, dThk/2, dThk/2, dVer/2);
-
-        return shape;
-    }
-
-    // create the tile (customised) logical volume
-    G4LogicalVolume* fLogTile(
+    G4LogicalVolume* fLogTile( // defined in DetectorConstruction.cc
         geomTrapezoid* pGeom, 
         G4String name, 
         G4double dThk,
         G4Material* pMaterial, 
         G4VisAttributes* pColour,
-
-        G4int signHalf = 0, // if 0, full tile is kept; if > (<) 0 , half tile is selected from positive (negative) half
-
-        G4double holeR = 0., // radius of the service holes - if 0, no holes are modelled
-        G4double holeX = 0., // hole centre, horizontal distance from tile centre
-        G4double holeY = 0. // hole centre, vertical distance from tile centre
-        // note: two holes are always added to the full tile: one in (holeX, holey), one in (-holeX, -holey)
-    ){
-        G4VSolid* shape_full = fShapeTileFull(pGeom, name, dThk);
-        G4VSolid* shape_fin;
-
-        G4String name_solid = name + "_Shape";
-
-        // model service holes
-        G4VSolid* shape_holes;
-        if (holeR > 0) {
-            G4RotationMatrix* holeRotation = new G4RotationMatrix();
-            holeRotation->rotateX(90 * deg);
-
-            G4VSolid* shape_hole = new G4Tubs("hole_Shape", 0., holeR, dThk / 2, 0., 2*acos(-1));
-
-            G4VSolid* shape_holes_temp = new G4SubtractionSolid(
-                "temp", shape_full, shape_hole, holeRotation, G4ThreeVector(holeX, 0., holeY)
-            );
-            shape_holes = new G4SubtractionSolid(
-                name_solid, shape_holes_temp, shape_hole, holeRotation, G4ThreeVector(-holeX, 0., -holeY)
-            );
-        }
-        shape_fin = holeR > 0 ? shape_holes : shape_full;
-        
-        // turn full tile into half tile
-        G4VSolid* shape_halved;
-        if (signHalf) {
-            G4int sign = (signHalf > 0) ? 1 : -1 ;
-
-            G4double dHor_b = pGeom->GetDHor_b();
-            G4double dHor_t = pGeom->GetDHor_t();
-            G4double dHor_max = dHor_t > dHor_b ? dHor_t : dHor_b ;
-            G4double dVer = pGeom->GetDVer();
-            G4VSolid* shape_rm = new G4Box("rm_Shape", dHor_max / 4, dThk / 2, dVer / 2);
-
-            G4VSolid* shape_intersect = new G4IntersectionSolid(
-                name_solid, shape_fin, shape_rm, nullptr, G4ThreeVector(sign*dHor_max/4, 0., 0.)
-            );
-
-            shape_halved = shape_intersect;
-        }
-        shape_fin = signHalf ? shape_halved : shape_fin ;
-
-        G4String name_log = name + "_Log";
-        G4LogicalVolume* logvol = new G4LogicalVolume(shape_fin, pMaterial, name_log);
-        logvol->SetVisAttributes(pColour);
-
-        return logvol;
-    }
+        G4int signHalf,
+        G4double holeR ,
+        G4double holeX ,
+        G4double holeY
+    );
 	
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 	
--- succosim/src/DetectorConstruction.cc
+++ succosim/src/DetectorConstruction.cc
@@ -77,6 +77,8 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 	geomTrapezoid* tileTestGeom = new geomTrapezoid(radius, height, angle);
     G4double offset;
 
+    // preliminary tests 0
+
     // // full tile
     // G4LogicalVolume* tileTest0Log = fLogTile(tileTestGeom, "tile0", thickness, bc400, cyan, 0, holeradius, 0, holey);
     // new G4PVPlacement(nullptr, G4ThreeVector(0, 0, 0), tileTest0Log, "tile0", worldLog_rot, false, 0);
@@ -126,7 +128,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
     // new G4PVPlacement(nullptr, G4ThreeVector(offset, -18*thickness, 0), tileTest9Log, "tile9", worldLog_rot, false, 0);
     // tileTestGeom->RmHorGaps();
 
-
+    // preliminary tests 1
 
     tileTestGeom->AddHorGaps(sidegap);
     offset = tileTestGeom->GetFullToHalfCentreOffset();
@@ -173,4 +175,106 @@ void DetectorConstruction::ConstructSDandField()
 // ============================================================
 // ============================================================
 // implement custom methods here
+
+// reduce the horizontal dimensions to account for the fibre gaps
+// only (some) horizontal quantities are affected
+void DetectorConstruction::geomTrapezoid::AddHorGaps(G4double gapsize) {
+    GapHor = gapsize / cos(GetTheta() / 2);
+    isGaps = true;
+
+    L_b = GetDHor_b() - 2*GapHor;
+    L_t = GetDHor_t() - 2*GapHor;
+    SetDHor_mid(); // recalculate L_mid
+}
+
+//
+void DetectorConstruction::geomTrapezoid::RmHorGaps() {
+    if (isGaps) {
+        L_b = GetDHor_b() + 2*GapHor;
+        L_t = GetDHor_t() + 2*GapHor;
+        SetDHor_mid(); // recalculate L_mid
+
+        GapHor = 0;
+        isGaps = false;
+    } else {return;}
+}
+
+// create the full tile solid
+G4VSolid* DetectorConstruction::fShapeTileFull(
+    geomTrapezoid* pGeom, 
+    G4String name, 
+    G4double dThk
+){
+    G4double dHor_b = pGeom->GetDHor_b();
+    G4double dHor_t = pGeom->GetDHor_t();
+    G4double dVer = pGeom->GetDVer();
+
+    G4String name_solid = name + "_Shape";
+    G4VSolid* shape = new G4Trd(name_solid, dHor_b/2, dHor_t/2, dThk/2, dThk/2, dVer/2);
+
+    return shape;
+}
+
+// create the tile (customised) logical volume
+G4LogicalVolume* DetectorConstruction::fLogTile(
+    geomTrapezoid* pGeom, 
+    G4String name, 
+    G4double dThk,
+    G4Material* pMaterial, 
+    G4VisAttributes* pColour,
+
+    G4int signHalf = 0, // if 0, full tile is kept; if > (<) 0 , half tile is selected from positive (negative) half
+
+    G4double holeR = 0., // radius of the service holes - if 0, no holes are modelled
+    G4double holeX = 0., // hole centre, horizontal distance from tile centre
+    G4double holeY = 0. // hole centre, vertical distance from tile centre
+    // note: two holes are always added to the full tile: one in (holeX, holey), one in (-holeX, -holey)
+){
+    G4VSolid* shape_full = fShapeTileFull(pGeom, name, dThk);
+    G4VSolid* shape_fin;
+
+    G4String name_solid = name + "_Shape";
+
+    // model service holes
+    G4VSolid* shape_holes;
+    if (holeR > 0) {
+        G4RotationMatrix* holeRotation = new G4RotationMatrix();
+        holeRotation->rotateX(90 * deg);
+
+        G4VSolid* shape_hole = new G4Tubs("hole_Shape", 0., holeR, dThk / 2, 0., 2*acos(-1));
+
+        G4VSolid* shape_holes_temp = new G4SubtractionSolid(
+            "temp", shape_full, shape_hole, holeRotation, G4ThreeVector(holeX, 0., holeY)
+        );
+        shape_holes = new G4SubtractionSolid(
+            name_solid, shape_holes_temp, shape_hole, holeRotation, G4ThreeVector(-holeX, 0., -holeY)
+        );
+    }
+    shape_fin = holeR > 0 ? shape_holes : shape_full;
+    
+    // turn full tile into half tile
+    G4VSolid* shape_halved;
+    if (signHalf) {
+        G4int sign = (signHalf > 0) ? 1 : -1 ;
+
+        G4double dHor_b = pGeom->GetDHor_b();
+        G4double dHor_t = pGeom->GetDHor_t();
+        G4double dHor_max = dHor_t > dHor_b ? dHor_t : dHor_b ;
+        G4double dVer = pGeom->GetDVer();
+        G4VSolid* shape_rm = new G4Box("rm_Shape", dHor_max / 4, dThk / 2, dVer / 2);
+
+        G4VSolid* shape_intersect = new G4IntersectionSolid(
+            name_solid, shape_fin, shape_rm, nullptr, G4ThreeVector(sign*dHor_max/4, 0., 0.)
+        );
+
+        shape_halved = shape_intersect;
+    }
+    shape_fin = signHalf ? shape_halved : shape_fin ;
+
+    G4String name_log = name + "_Log";
+    G4LogicalVolume* logvol = new G4LogicalVolume(shape_fin, pMaterial, name_log);
+    logvol->SetVisAttributes(pColour);
+
+    return logvol;
+}
 
